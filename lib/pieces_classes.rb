4c288caac3c37995bf3400b0cec25afb323de4d2
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-03-20 21:36:19 +0100

    Added basic moves for Pawn class

--- lib/pieces_classes.rb
+++ lib/pieces_classes.rb
@@ -8,6 +8,19 @@ class Pawn
     @color = color
     @position = position
   end
+  def possible_moves
+    possible = []
+    if color == :white
+      if (@position[0] >= 0 && @position[0] < 7)
+        possible << [@position[0]+1, @position[1]]
+      end
+    else
+      if (@position[0] > 0 && @position[0] <= 7)
+        possible << [@position[0]-1, @position[1]]
+      end
+    end
+    possible
+  end
 end
 
 pawn = Pawn.new :white, [1,1]
--- spec/pieces_spec.rb
+++ spec/pieces_spec.rb
@@ -6,5 +6,13 @@ describe Pawn do
       pawn = Pawn.new :white, [1,1]
       expect(pawn.possible_moves).to eql([[2,1]]) 
     end
+    it "Suggests moves backwards if it's black" do
+      pawn = Pawn.new :black, [1,1]
+      expect(pawn.possible_moves).to eql([[0,1]])
+    end
+    it "Doesn't suggest anything if against the wall" do
+      pawn = Pawn.new :white, [7,2]
+      expect(pawn.possible_moves).to eql([])
+    end
   end
 end
